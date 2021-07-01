ab6295b87170a1d6ab90945272a9e442da647acd
Gayane Ghazaryan <74008543+gayaneghazaryan@users.noreply.github.com>
2021-07-01 19:34:31 +0400

    Second Commit

--- src/EmployeeService.java
+++ src/EmployeeService.java
@@ -1,43 +1,58 @@
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Marshaller;
-import jakarta.xml.bind.Unmarshaller;
-
-import java.io.File;
+import java.io.*;
 
 public class EmployeeService {
 
     private static final String FILE_NAME = "src\\employee.xml";
 
     public static void serializeEmployee(Employee employee) {
-        JAXBContext jaxbContext;
-        Marshaller marshaller;
-        try {
-            File file = new File(FILE_NAME);
-            jaxbContext = JAXBContext.newInstance(Employee.class);
-            marshaller = jaxbContext.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,true);
-            marshaller.marshal(employee,file);
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
 
-        } catch (JAXBException e) {
+        try {
+            fos = new FileOutputStream(FILE_NAME);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(employee);
+        }catch(Exception e) {
+            e.printStackTrace();
+        } finally{
+            try {
+                if(oos != null) oos.close();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            if (fos != null) fos.close();
+        }catch(IOException e) {
             e.printStackTrace();
         }
     }
 
     public static Employee deSerializeEmployee() {
-        JAXBContext jaxbContext ;
-        Unmarshaller unmarshaller;
         Employee employee = null;
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
         try {
-            File file = new File(FILE_NAME);
-            jaxbContext = JAXBContext.newInstance(Employee.class);
-
-            unmarshaller = jaxbContext.createUnmarshaller();
-
-            employee = (Employee) unmarshaller.unmarshal(file);
-
-        } catch (JAXBException e) {
+            fis = new FileInputStream(FILE_NAME);
+            ois = new ObjectInputStream(fis);
+            Object object = ois.readObject();
+            employee = (Employee) object;
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if(ois != null) ois.close();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(fis != null) {
+                    fis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return employee;
     }
